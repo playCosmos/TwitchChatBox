47927fddd1f802bd723885b450934190c26cbc0c
JunHyeonB <wkit.jhbae@gmail.com>
2023-07-11 14:18:24 +0900

    update 20230711

--- twitchChat.js
+++ twitchChat.js
@@ -22,15 +22,28 @@ ComfyJS.onChat = (user, message, flags, self, extra) => {
     if (flags.broadcaster) {
         chatHtml += '<img src="https://static-cdn.jtvnw.net/badges/v1/5527c58c-fb7d-422d-b71b-f309dcb85cc1/1" alt="" class="badge">';
     }
-    if (flags.moderator) {
+    if (flags.mod) {
         chatHtml += '<img src="https://static-cdn.jtvnw.net/badges/v1/3267646d-33f0-4b17-b3df-f923a41db1d0/1" alt="" class="badge">';
     }
+    if (flags.vip) {
+        chatHtml += '<img src="https://static-cdn.jtvnw.net/badges/v1/b817aba4-fad8-49e2-b88a-7cc744dfa6ec/3" alt="" class="badge">';
+    }
+    if (flags.customReward) {
+        //커스텀 뱃지
+    }
     if (flags.subscriber) {
-        chatHtml += '<img src="' + badgeData[extra.userBadges.subscriber].image_url_1x + '" alt="" class="twitch badge_broadcaster badge">';
+        try {
+            chatHtml += '<img src="' + badgeData[extra.userBadges.subscriber].image_url_4x + '" alt="" class="twitch badge_broadcaster badge">';
+        } catch (e) {
+            console.log(e);
+        }
     }
     chatHtml += '<span class="name">' + user + '</span>';
     chatHtml += '<span class="dot"> : </span>';
     chatHtml += '</p>';
+
+    console.log(extra)
+
     if (extra.messageEmotes === null) {
         chatHtml += '<p class="text">' + message + '</p>';
     } else {
@@ -47,7 +60,7 @@ ComfyJS.onChat = (user, message, flags, self, extra) => {
 
             stringReplacements.push({
                 stringToReplace: stringToReplace,
-                replacement: `<img class="emote" src="https://static-cdn.jtvnw.net/emoticons/v1/${id}/1.0">`,
+                replacement: '<img class="emote" src="https://static-cdn.jtvnw.net/emoticons/v2/${id}/default/dark/1.0">',
             });
         });
         const messageHTML = stringReplacements.reduce(
@@ -79,6 +92,9 @@ $(document).ready(function () {
             'grant_type': 'client_credentials'
         },
         success: function (result) {
+            console.log("token")
+            console.log(result)
+
             accessToken = result.access_token;
             tokenType = result.token_type;
 
@@ -90,7 +106,14 @@ $(document).ready(function () {
                     'Authorization': 'Bearer ' + accessToken
                 },
                 success: function (result) {
-                    broadcasterId = result.data[0].id;
+                    console.log("channels")
+                    console.log(result)
+
+                    for (let i = 0 ; i < result.data.length ; i++){
+                        if (result.data[i].broadcaster_login === broadcasterLogin)
+                            broadcasterId =result.data[i].id;
+                    }
+
 
                     $.ajax({
                         url: apiUrl + '/helix/chat/badges?broadcaster_id=' + broadcasterId,
@@ -100,13 +123,18 @@ $(document).ready(function () {
                             'Authorization': 'Bearer ' + accessToken
                         },
                         success: function (result) {
+                            console.log("badges")
                             console.log(result)
-                            for (let i = 0; i < result.data[0].versions.length; i++) {
-                                badgeData[result.data[0].versions[i].id] = {
-                                    'image_url_1x': result.data[0].versions[i].image_url_1x,
-                                    'image_url_2x': result.data[0].versions[i].image_url_2x,
-                                    'image_url_3x': result.data[0].versions[i].image_url_3x
+                            try {
+                                for (let i = 0; i < result.data[0].versions.length; i++) {
+                                    badgeData[result.data[0].versions[i].id] = {
+                                        'image_url_1x': result.data[0].versions[i].image_url_1x,
+                                        'image_url_2x': result.data[0].versions[i].image_url_2x,
+                                        'image_url_4x': result.data[0].versions[i].image_url_4x
+                                    }
                                 }
+                            } catch (e) {
+                                console.log(e);
                             }
                         },
                         error: function (error) {
